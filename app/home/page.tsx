4e0c6096ba8afb2e0c82918b7a4aaf510bd50f6f
sakhiy <sakhiyfairuz@gmail.com>
2023-08-11 10:10:51 +0700

    fix to responsive desktop page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -5,9 +5,10 @@ import LinkCards from "@/components/linkCards"
 const Home = () => {
     return (
         <div className="w-full min-h-screen flex flex-row bg-neutral-950 text-white">
-            <div className="w-1/12">
+            <div className="2xl:visible invisible 2xl:w-1/12">
+                
             </div>
-            <div className="flex flex-col h-screen w-3/12 p-10">
+            <div className="flex flex-col h-screen w-4/12 2xl:w-3/12 p-10">
                 <div className="flex-none">
                     <div className="text-5xl font-bold">
                         Twitter
@@ -22,9 +23,9 @@ const Home = () => {
                     <UserCard name="fairuz" username="@fairuz" photo="placeholder.jpg" />
                 </div>
             </div>
-            <div className="w-5/12 border border-neutral-700 min-s-screen justify-center p-5">This is gonna be the main feed</div>
-            <div className="w-3/12 min-s-screen justify-start text-left p-5">this is gonna be trending and about</div>
-            <div className="w-1/12">
+            <div className="w-5/12 border border-neutral-700  justify-center p-5">This is gonna be the main feed</div>
+            <div className="w-4/12 2xl:w-3/12 justify-start text-left p-5">this is gonna be trending and about asdfafsdasdfasdfasdf asdf asdf asdf asdf asdf asfd asdf asdf asdf asdf asdf </div>
+            <div className="2xl:visible invisible  2xl:w-1/12">
 
             </div>
         </div> 
